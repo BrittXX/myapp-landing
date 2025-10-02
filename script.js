08cae0332d2c347bc56f7f2469f05ee1bd182027
BrittXX <ufukrtls@gmail.com>
2025-10-02 23:00:05 +0300

    degisiklik: hero arka plan degrade eklendi

---
+++ script.js
@@ -0,0 +1,26 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "🌞";
+  } else {
+    toggleBtn.textContent = "🌙";
+  }
+});
+// Fade-in animasyonu
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, { threshold: 0.2 });
+
+sections.forEach(section => {
+  observer.observe(section);
+})
